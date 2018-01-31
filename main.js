6dcbc6cd5430b4837afe0f9dac00edd7e984c2ae
zdzialowska <z.dzialowska@gmail.com>
2018-01-31 21:11:47 +0100

    modal once opened can't be closed still

--- main.js
+++ main.js
@@ -36,24 +36,110 @@ let closeBtn = document.getElementsByClassName('closeBtn')[0];
 
 // Listen for open click
 modalBtn1.addEventListener('click', openModal);
+modalBtn2.addEventListener('click', openModal);
+modalBtn3.addEventListener('click', openModal);
+modalBtn4.addEventListener('click', openModal);
+modalBtn5.addEventListener('click', openModal);
+modalBtn6.addEventListener('click', openModal);
+modalBtn7.addEventListener('click', openModal);
+modalBtn8.addEventListener('click', openModal);
+modalBtn9.addEventListener('click', openModal);
+modalBtn10.addEventListener('click', openModal);
+modalBtn11.addEventListener('click', openModal);
+modalBtn12.addEventListener('click', openModal);
+modalBtn13.addEventListener('click', openModal);
+modalBtn14.addEventListener('click', openModal);
+modalBtn15.addEventListener('click', openModal);
+
 // Listen for close click
 closeBtn.addEventListener('click', closeModal);
+
 // Listen for outside click
 window.addEventListener('click', outsideClick);
 
 // Function to open modal
 function openModal(){
-  modal.style.display = 'block';
+  modal1.style.display = 'block';
+    modal2.style.display = 'block';
+    modal3.style.display = 'block';
+    modal4.style.display = 'block';
+    modal5.style.display = 'block';
+    modal6.style.display = 'block';
+    modal7.style.display = 'block';
+    modal8.style.display = 'block';
+    modal9.style.display = 'block';
+    modal10.style.display = 'block';
+    modal11.style.display = 'block';
+    modal12.style.display = 'block';
+    modal13.style.display = 'block';
+    modal14.style.display = 'block';
+    modal15.style.display = 'block';
 }
 
 // Function to close modal
 function closeModal(){
-  modal.style.display = 'none';
+  modal1.style.display = 'none';
+    modal2.style.display = 'none';
+    modal3.style.display = 'none';
+    modal4.style.display = 'none';
+    modal5.style.display = 'none';
+    modal6.style.display = 'none';
+    modal7.style.display = 'none';
+    modal8.style.display = 'none';
+    modal9.style.display = 'none';
+    modal10.style.display = 'none';
+    modal11.style.display = 'none';
+    modal12.style.display = 'none';
+    modal13.style.display = 'none';
+    modal14.style.display = 'none';
+    modal15.style.display = 'none';
 }
 
 // Function to close modal if outside click
 function outsideClick(e){
-  if(e.target == modal){
-    modal.style.display = 'none';
+  if(e.target == modal1){
+    modal1.style.display = 'none';
+  }
+    if(e.target == modal2){
+    modal2.style.display = 'none';
+    }
+    if(e.target == modal3){
+    modal3.style.display = 'none';
+    }
+    if(e.target == modal4){
+    modal4.style.display = 'none';
+    }
+    if(e.target == modal5){
+    modal5.style.display = 'none';
+    }
+    if(e.target == modal6){
+    modal6.style.display = 'none';
+    }
+    if(e.target == modal7){
+    modal7.style.display = 'none';
+    }
+    if(e.target == modal8){
+    modal8.style.display = 'none';
+    }
+    if(e.target == modal9){
+    modal9.style.display = 'none';
+    }
+    if(e.target == modal10){
+    modal10.style.display = 'none';
+    }
+    if(e.target == modal11){
+    modal11.style.display = 'none';
+    }
+    if(e.target == modal12){
+    modal12.style.display = 'none';
+    }
+    if(e.target == modal13){
+    modal13.style.display = 'none';
+    }
+    if(e.target == modal14){
+    modal14.style.display = 'none';
+    }
+    if(e.target == modal15){
+    modal15.style.display = 'none';
   }
 }
